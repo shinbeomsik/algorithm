52e0068eab07951ef9fa15d9d7e74462278b289e
shinbeomsik <tnghrltkwnst@naver.com>
2023-09-09 23:51:26 +0900

    알고리즘 0레벨 풀이 (다른버젼)
    속도가 더 빠를줄 알았으나 아니였음..

--- ds00/src/프로그래머스풀기레벨0/직각삼각형출력하기.java
+++ ds00/src/프로그래머스풀기레벨0/직각삼각형출력하기.java
@@ -7,12 +7,11 @@ public class 직각삼각형출력하기 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		StringBuilder arr = new StringBuilder();
 		
 		for(int i=0; i<n;i++) {
-			for(int j=0; j<i+1; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
+				arr.append("*");
+				System.out.println(arr);
 		}
 
 	}
