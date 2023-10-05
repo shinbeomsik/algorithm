b5d0a8133bf452d5937573275809e909c65d0913
shinbeomsik <tnghrltkwnst@naver.com>
2023-10-05 19:46:03 +0900

    알고리즘 0레벨 풀이

--- ds00/src/프로그래머스풀기레벨0/유한소수판별하기.java
+++ ds00/src/프로그래머스풀기레벨0/유한소수판별하기.java
@@ -1,36 +1,42 @@
 package 프로그래머스풀기레벨0;
 
 import java.util.*;
+
 public class 유한소수판별하기 {
 
 	public static void main(String[] args) {
 		Solution170 sol = new Solution170();
-		int a =12; 
-		int b =21;
+		int a = 7;
+		int b = 20;
 
 		int answer = sol.solution(a, b);
 		System.out.println(answer);
 
 	}
-}  
+}
 
 class Solution170 {
-	public int solution(int a, int b) {
-        int answer = 0;
-        
-        int c = a;
-        int d = b;
-        if(c>d) {
-        	for(int i=1;i<=a;i++) {
-        		if(c%i==0 &&d%i==0) {
-        			c = c/i;
-        			d = d/i;
-        		}
-        	}
-        }
-        
-        System.out.println(c);
-        System.out.println(d);
-        return answer;
-    }
+	 public int solution(int a, int b) {
+
+	        int B = b / GCD(a,b);
+	        while (B != 1) {
+	            if (B % 2 == 0){
+	                B = B / 2;
+	            } else if (B % 5 == 0) {
+	                B = B / 5;
+	            } else {
+	                return 2;
+	            }
+	        }
+	        return 1;
+	    }
+	    
+	    // 유클리드 호제법! 두 수의 최대 공약수 구하기
+	    private int GCD(int a, int b) {
+	        if (b == 0) {
+	            return a;
+	        } else {
+	            return GCD(b, a % b);
+	        }
+	    }
 }
