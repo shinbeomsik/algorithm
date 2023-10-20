372537faeb2aff955115e22d0168dcaf220a9178
shinbeomsik <tnghrltkwnst@naver.com>
2023-10-20 22:29:05 +0900

    알고리즘 1레벨 풀이 복습1회차

---
+++ src/ds00/src/프로그래머스풀기레벨1_1회차반복/옹알이2.java
@@ -0,0 +1,39 @@
+package 프로그래머스풀기레벨1_1회차반복;
+
+
+public class 옹알이2 {
+
+	public static void main(String[] args) {
+		Solution54 sol = new Solution54();
+		String[] babbling = {};
+
+		int answer = sol.solution(babbling);
+		System.out.println(answer);
+
+	} 
+}
+
+class Solution54 {
+	 public int solution(String[] babbling) {
+	        int answer = 0;
+	        for(int i=0;i<babbling.length;i++){
+	            babbling[i] = babbling[i].replaceAll("ayaaya","x");
+	            babbling[i] = babbling[i].replaceAll("yeye","x");
+	            babbling[i] = babbling[i].replaceAll("woowoo","x");
+	            babbling[i] = babbling[i].replaceAll("mama","x");
+	        }
+	        for(int i=0;i<babbling.length;i++){
+	            babbling[i] = babbling[i].replaceAll("aya"," ");
+	            babbling[i] = babbling[i].replaceAll("ye"," ");
+	            babbling[i] = babbling[i].replaceAll("woo"," ");
+	            babbling[i] = babbling[i].replaceAll("ma"," ");
+	        }
+	        
+	        for(int i=0;i<babbling.length;i++){
+	            if(babbling[i].trim().length()==0){
+	                answer++;
+	            }
+	        }
+	        return answer;
+	    }
+}
