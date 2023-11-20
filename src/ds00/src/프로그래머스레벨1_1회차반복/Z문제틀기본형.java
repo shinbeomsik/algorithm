fc656588e7d262b8203f742c148bc1c6e1357818
shinbeomsik <tnghrltkwnst@naver.com>
2023-11-20 17:48:21 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_1회차반복/Z문제틀기본형.java
@@ -0,0 +1,12 @@
+package 프로그래머스레벨1_1회차반복;
+
+
+public class Z문제틀기본형 {
+}
+
+class Solution {
+	public int solution(int angle) {
+		int answer = 0;
+		return answer;
+	}
+}
---
+++ src/ds00/src/프로그래머스레벨1_1회차반복/약수의합.java
@@ -0,0 +1,17 @@
+package 프로그래머스레벨1_1회차반복;
+
+
+public class 약수의합 {
+}
+
+class Solution6 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                answer = answer + i;
+            }
+        }
+        return answer;
+    }
+}
