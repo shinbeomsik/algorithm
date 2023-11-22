76c067a66a15c556f6cdca0e6ed701490ececbe9
shinbeomsik <tnghrltkwnst@naver.com>
2023-11-23 08:51:25 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복/삼총사.java
@@ -0,0 +1,24 @@
+package 프로그래머스레벨1_반복;
+
+
+public class 삼총사 {
+}
+
+class Solution33 {
+    public int solution(int[] number) {
+        int answer = 0;
+        for(int i=0;i<number.length-2;i++){
+            int sum =0;
+            for(int j=i+1; j<number.length-1;j++){
+                for(int k=j+1;k<number.length;k++){
+                    sum = number[i] + number[j] + number[k];
+                    
+                    if(sum==0){
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
