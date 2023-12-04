9397db7cc9f17ea495c615de523137f320913bc5
shinbeomsik <tnghrltkwnst@naver.com>
2023-12-04 10:28:45 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복/명예의전당1.java
@@ -0,0 +1,35 @@
+package 프로그래머스레벨1_반복;
+import java.util.*;
+
+public class 명예의전당1 {
+	
+}
+
+class Solution43 {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        
+        List<Integer> arr = new ArrayList<>();
+       if(k<=score.length){ 
+            for(int i=0;i<k;i++){
+                arr.add(score[i]);
+                Collections.sort(arr);
+                answer[i] = arr.get(0);
+            }
+        
+            for(int i=k; i<score.length;i++){
+                arr.add(score[i]);
+                Collections.sort(arr);
+                arr.remove(0);
+                answer[i] = arr.get(0);
+            }
+       }else{
+           for(int i=0;i<score.length;i++){
+               arr.add(score[i]);
+               Collections.sort(arr);
+               answer[i] = arr.get(0);
+           }
+       }
+        return answer;
+    }
+}
---
+++ src/ds00/src/프로그래머스레벨1_반복/완주하지못한선수.java
@@ -0,0 +1,29 @@
+package 프로그래머스레벨1_반복;
+
+import java.util.*;
+
+public class 완주하지못한선수 {
+	
+}
+
+class Solution45 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        if(participant.length>1){
+        for(int i=0;i<completion.length;i++){
+            if(!participant[i].equals(completion[i])){
+                answer = participant[i];
+                break;
+            }
+            answer =  participant[participant.length-1];
+        }
+        }else{
+            answer = participant[0];
+        }
+        return answer;
+    }
+}
