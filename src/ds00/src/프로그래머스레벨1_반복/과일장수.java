a7c7a9104dd0ea6fcc5e742c72d5b87473248b64
shinbeomsik <tnghrltkwnst@naver.com>
2023-12-11 08:46:43 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복/과일장수.java
@@ -0,0 +1,25 @@
+package 프로그래머스레벨1_반복;
+
+import java.util.*;
+public class 과일장수 {
+	
+}
+
+class Solution47 {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        
+        List<Integer> arr = new ArrayList<>();
+        Arrays.sort(score);
+        for(int i=score.length-1;i>=0;i--){
+
+            arr.add(score[i]);
+            if(arr.size() == m){
+                answer = answer + arr.get(arr.size()-1) * m ;
+                arr.clear();
+            }
+        }
+
+        return answer;
+    }
+}
