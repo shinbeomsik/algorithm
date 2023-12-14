f2363165a915894b546e4489c801f48cfce26fe2
shinbeomsik <tnghrltkwnst@naver.com>
2023-12-14 14:18:49 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복/a2016년.java
@@ -0,0 +1,20 @@
+package 프로그래머스레벨1_반복;
+
+import java.time.*;
+public class a2016년 {
+	
+}
+
+class Solution52 {
+    public String solution(int a, int b) {
+        String answer = "";
+        LocalDate specificDate = LocalDate.of(2016, a, b);
+        
+        DayOfWeek dayOfWeek = specificDate.getDayOfWeek();
+        
+        String dayOfWeekString = dayOfWeek.toString();
+        
+        answer = dayOfWeekString.substring(0,3);
+        return answer;
+    }
+}
