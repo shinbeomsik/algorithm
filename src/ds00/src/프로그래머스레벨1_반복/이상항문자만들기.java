7b41238733a1b4141bb13632e8a14f03c5bd4212
shinbeomsik <tnghrltkwnst@naver.com>
2023-12-18 16:51:59 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복/이상항문자만들기.java
@@ -0,0 +1,28 @@
+package 프로그래머스레벨1_반복;
+
+
+public class 이상항문자만들기 {
+	
+}
+
+class Solution50 {
+	  public String solution(String s) {
+	        String answer = "";
+	        String[] k = s.split(" ", -1);
+	        
+	        for(int i=0; i<k.length; i++){
+	            String[] q = k[i].split("");
+	            for(int j=0; j<q.length; j++){
+	                if(j%2==0){
+	                    q[j] = q[j].toUpperCase();
+	                }else{
+	                    q[j] = q[j].toLowerCase();
+	                }
+	            }
+	            k[i] = String.join("",q);
+	        }
+	        
+	        answer = String.join(" " , k);
+	        return answer;
+	    }
+}
