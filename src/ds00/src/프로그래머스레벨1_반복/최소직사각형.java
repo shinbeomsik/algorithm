159c19856844a6ad9c70b213a21c0d1992f354d4
shinbeomsik <tnghrltkwnst@naver.com>
2023-12-19 09:50:33 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복/최소직사각형.java
@@ -0,0 +1,25 @@
+package 프로그래머스레벨1_반복;
+import java.util.*;
+
+public class 최소직사각형 {
+	
+}
+
+class Solution51 {
+    public int solution(int[][] sizes) {
+        int answer = 0; 
+        int maxW =0;
+        int maxH =0;
+        for(int i=0;i<sizes.length;i++){
+            Arrays.sort(sizes[i]);
+            if(sizes[i][0] > maxW){
+                maxW = sizes[i][0];
+            }
+            if(sizes[i][1] > maxH){
+                maxH = sizes[i][1];
+            }
+        }
+        answer = maxW * maxH;
+        return answer;
+    }
+}
