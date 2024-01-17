74508e0be9a6f9391f13609b4431749b6b9e776a
shinbeomsik <tnghrltkwnst@naver.com>
2024-01-18 01:48:51 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복2/삼총사.java
@@ -0,0 +1,22 @@
+package 프로그래머스레벨1_반복2;
+
+
+public class 삼총사 {
+	   
+}
+
+class Solution36 {
+	public int solution(int[] number) {
+        int answer = 0;
+        for(int i=0;i<number.length;i++){
+            for(int j=i+1; j<number.length;j++){
+                for(int k=j+1; k<number.length;k++){
+                    if(number[i] + number[j] + number[k] == 0){
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
---
+++ src/ds00/src/프로그래머스레벨1_반복2/서울에서김서방찾기.java
@@ -0,0 +1,19 @@
+package 프로그래머스레벨1_반복2;
+
+
+public class 서울에서김서방찾기 {
+	   
+}
+
+class Solution16 {
+	public String solution(String[] seoul) {
+        String answer = "";
+        for(int i=0;i<seoul.length;i++){
+            if(seoul[i].equals("Kim")){
+                answer = "김서방은 " + i + "에 있다";
+                break;
+            }
+        }
+        return answer;
+    }
+}
