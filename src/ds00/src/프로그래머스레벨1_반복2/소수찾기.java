17cf68515ac45200af822f74e2651ebd8fc2b931
shinbeomsik <tnghrltkwnst@naver.com>
2024-01-31 19:55:50 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복2/소수찾기.java
@@ -0,0 +1,24 @@
+package 프로그래머스레벨1_반복2;
+
+
+public class 소수찾기 {
+	   
+}
+
+class Solution54 {
+	 public int solution(int n) {
+	        int answer = 0;
+	        int[] arr = new int[n+1];
+	        for(int i=1; i<arr.length;i++){
+	            for(int j=i; j<arr.length;j = j+i){
+	                arr[j]++;
+	            }
+	        }
+	        for(int i=2;i<arr.length;i++){
+	            if(arr[i] ==2){
+	                answer++;
+	            }
+	        }
+	        return answer;
+	    }
+}
---
+++ src/ds00/src/프로그래머스레벨1_반복2/수박수박수박수박수.java
@@ -0,0 +1,20 @@
+package 프로그래머스레벨1_반복2;
+
+
+public class 수박수박수박수박수 {
+	   
+}
+
+class Solution23 {
+	public String solution(int n) {
+        String answer = "";
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                answer = answer + "수";
+            }else{
+                answer = answer + "박";
+            }
+        }
+        return answer;
+    }
+}
