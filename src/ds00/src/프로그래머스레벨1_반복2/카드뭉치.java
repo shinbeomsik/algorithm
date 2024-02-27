c5b052b87ca42dd572e4f4e2e7e65beda4f59118
shinbeomsik <tnghrltkwnst@naver.com>
2024-02-27 19:27:05 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복2/카드뭉치.java
@@ -0,0 +1,25 @@
+package 프로그래머스레벨1_반복2;
+
+
+public class 카드뭉치 {
+	   
+}
+
+class Solution48 {
+	  public String solution(String[] cards1, String[] cards2, String[] goal) {
+	        String answer = "No";
+	        int count1 =0;
+	        int count2 =0;
+	        for(int i=0;i<goal.length;i++){
+	            if(count1 < cards1.length && goal[i].equals(cards1[count1])){
+	                count1++;
+	            }else if(count2 < cards2.length &&goal[i].equals(cards2[count2])){
+	                count2++;
+	            }
+	        }
+	        if(count1 + count2 == goal.length){
+	            answer = "Yes";
+	        }
+	        return answer;
+	    }
+}
---
+++ src/ds00/src/프로그래머스레벨1_반복2/콜라문제.java
@@ -0,0 +1,17 @@
+package 프로그래머스레벨1_반복2;
+
+
+public class 콜라문제 {
+	   
+}
+
+class Solution45 {
+	 public int solution(int a, int b, int n) {
+	        int answer = 0;
+	        while(n>=a){
+	            answer = answer + n/a *b;
+	            n = n%a + (n/a) *b;
+	        }
+	        return answer;
+	    }
+}
