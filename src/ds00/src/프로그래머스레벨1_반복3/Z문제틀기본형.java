07e2df84613bc485442248cbee6bd3f766c1f211
shinbeomsik <tnghrltkwnst@naver.com>
2024-03-07 00:32:00 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복3/Z문제틀기본형.java
@@ -0,0 +1,14 @@
+package 프로그래머스레벨1_반복3;
+
+
+public class Z문제틀기본형 {
+	   
+}
+
+class Solution {
+	public int solution(int angle) {
+		int answer = 0;
+		
+		return answer;
+	}
+}
---
+++ src/ds00/src/프로그래머스레벨1_반복3/나누어떨어지는숫자배열.java
@@ -0,0 +1,24 @@
+package 프로그래머스레벨1_반복3;
+import java.util.*;
+
+public class 나누어떨어지는숫자배열 {
+	   
+}
+
+class Solution14 {
+	 public List<Integer> solution(int[] arr, int divisor) {
+	        List<Integer> answer = new ArrayList<>(); 
+	        Arrays.sort(arr);
+	        for(int i=0; i<arr.length; i++){
+	           if(arr[i]%divisor ==0){
+	               answer.add(arr[i]);
+	           } 
+	        }
+	        
+
+	        if(answer.size() ==0){
+	            answer.add(-1);
+	        }
+	        return answer;
+	    }
+}
