140ad1c0b258ebbc590037896f6efebc315fe8a3
shinbeomsik <tnghrltkwnst@naver.com>
2024-03-08 21:52:01 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복3/나머지가1이되는수찾기.java
@@ -0,0 +1,20 @@
+package 프로그래머스레벨1_반복3;
+
+
+public class 나머지가1이되는수찾기 {
+	   
+}
+
+class Solution5 {
+	 public int solution(int n) {
+	        int answer = 0;
+	        
+	        for(int i=1; i <=n ; i++){
+	            if(n%i == 1){
+	                answer = i;
+	                break;
+	            }
+	        }
+	        return answer;
+	    }
+}
