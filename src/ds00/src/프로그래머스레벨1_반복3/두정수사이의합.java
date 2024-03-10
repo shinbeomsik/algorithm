6c06bab8f5bb7c8a4ea31289a423835bf07b377c
shinbeomsik <tnghrltkwnst@naver.com>
2024-03-10 19:46:16 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복3/두정수사이의합.java
@@ -0,0 +1,23 @@
+package 프로그래머스레벨1_반복3;
+import java.util.*;
+
+public class 두정수사이의합 {
+	   
+}
+
+class Solution9 {
+	  public long solution(int a, int b) {
+	        long answer = 0;
+	        
+	        int[] arr = new int[2];
+	        arr[0] = a;
+	        arr[1] = b;
+	        
+	        Arrays.sort(arr);
+	        
+	        for(int i= arr[0] ; i<=arr[1];i++){
+	            answer = answer + i;
+	        }
+	        return answer;
+	    }
+}
