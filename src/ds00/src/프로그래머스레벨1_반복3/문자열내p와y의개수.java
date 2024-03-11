2ee15a4447dd9b9cb1029834cd682e5aaa925af5
shinbeomsik <tnghrltkwnst@naver.com>
2024-03-11 19:14:53 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복3/문자열내p와y의개수.java
@@ -0,0 +1,27 @@
+package 프로그래머스레벨1_반복3;
+
+
+public class 문자열내p와y의개수 {
+	   
+}
+
+class Solution8 {
+	boolean solution(String s) {
+        boolean answer = true;
+        s  = s.toLowerCase();
+        int pcount = 0;
+        int ycount = 0;
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == 'p'){
+                pcount++;
+            }else if(s.charAt(i)== 'y'){
+                ycount++;
+            }
+        }
+        if(pcount != ycount){
+            answer = false;
+        }
+        return answer;
+    }
+}
