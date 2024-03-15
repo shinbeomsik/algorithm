47987b09d01a6adba47e0817ce692cc1be18f479
shinbeomsik <tnghrltkwnst@naver.com>
2024-03-15 17:12:12 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복3/없는숫자더하기.java
@@ -0,0 +1,16 @@
+package 프로그래머스레벨1_반복3;
+
+
+public class 없는숫자더하기 {
+	   
+}
+
+class Solution13 {
+	 public int solution(int[] numbers) {
+	        int answer = 45;
+	        for(int i=0; i<numbers.length; i++){
+	            answer = answer - numbers[i];
+	        }
+	        return answer;
+	    }
+}
---
+++ src/ds00/src/프로그래머스레벨1_반복3/예산.java
@@ -0,0 +1,23 @@
+package 프로그래머스레벨1_반복3;
+import java.util.*;
+
+public class 예산 {
+	   
+}
+
+class Solution25 {
+	public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        
+        int total = 0;
+        for(int i=0;i<d.length;i++){
+            total = total + d[i];
+            if(total > budget){
+                break;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
