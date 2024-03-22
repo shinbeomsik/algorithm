b02f0e60e99ef67ba9cfdc0c62ee40c917b25a4a
shinbeomsik <tnghrltkwnst@naver.com>
2024-03-22 23:13:01 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복3/과일장수.java
@@ -0,0 +1,17 @@
+package 프로그래머스레벨1_반복3;
+
+import java.util.*;
+public class 과일장수 {
+	   
+}
+
+class Solution59 {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;        
+        Arrays.sort(score);
+        for(int i=score.length-m; 0<=i;i = i-m){
+                answer = answer + score[i]*m;
+        }
+        return answer;
+    }
+}
---
+++ src/ds00/src/프로그래머스레벨1_반복3/모의고사.java
@@ -0,0 +1,46 @@
+package 프로그래머스레벨1_반복3;
+
+import java.util.*;
+public class 모의고사 {
+	   
+}
+
+class Solution58 {
+	public List<Integer> solution(int[] answers) {
+        List<Integer> answer = new ArrayList<>();
+        
+        int[] role1 = {1,2,3,4,5};
+        int[] role2 = {2,1,2,3,2,4,2,5};
+        int[] role3 = {3,3,1,1,2,2,4,4,5,5};
+        
+        int[] total = new int[4];
+        int[] total2 = new int[4];
+        
+        for(int i=0; i<answers.length;i++){
+            if(answers[i] == role1[i%5]){
+                total[1]++;
+                total2[1]++;
+            }
+            
+            if(answers[i] == role2[i%8]){
+                total[2]++;
+                total2[2]++;
+            }
+            
+            if(answers[i] == role3[i%10]){
+                total[3]++;
+                total2[3]++;
+            }
+        }
+        
+        Arrays.sort(total);
+        
+        for(int i=1;i<total.length;i++){
+            if(total[3] ==total2[i]){
+                answer.add(i);
+            }
+        }
+
+        return answer;
+    }
+}
