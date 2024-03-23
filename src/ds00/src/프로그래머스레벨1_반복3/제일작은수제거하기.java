fe7e21c3cdadf767de8d38b3d8ffe17db1ac4d83
shinbeomsik <tnghrltkwnst@naver.com>
2024-03-23 23:22:59 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복3/제일작은수제거하기.java
@@ -0,0 +1,22 @@
+package 프로그래머스레벨1_반복3;
+
+import java.util.*;
+public class 제일작은수제거하기 {
+	   
+}
+
+class Solution35 {
+	public List<Integer> solution(int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        if(arr.length ==1){
+            answer.add(-1);
+        }else{
+            for(int i=0;i<arr.length;i++){
+                answer.add(arr[i]);
+            }
+            Arrays.sort(arr);
+            answer.remove(Integer.valueOf(arr[0]));
+        }
+        return answer;
+    }
+}
