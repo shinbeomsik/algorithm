555b10985149f64596878ba8ef1aca55c119bc58
shinbeomsik <tnghrltkwnst@naver.com>
2024-03-24 00:11:32 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복3/정수내림차순으로배치.java
@@ -0,0 +1,18 @@
+package 프로그래머스레벨1_반복3;
+
+import java.util.*;
+public class 정수내림차순으로배치 {
+	   
+}
+
+class Solution34 {
+	public long solution(long n) {
+        long answer = 0;
+        String k = Long.toString(n);
+        String[] arr = k.split("");
+        Arrays.sort(arr, Comparator.reverseOrder());
+        k = String.join("",arr);
+        answer = Long.parseLong(k);
+        return answer;
+    }
+}
