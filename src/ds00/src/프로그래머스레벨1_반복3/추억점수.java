3e0e9405e3437e348df455104e3572e70d2300a7
shinbeomsik <tnghrltkwnst@naver.com>
2024-03-25 19:59:47 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복3/추억점수.java
@@ -0,0 +1,23 @@
+package 프로그래머스레벨1_반복3;
+
+public class 추억점수 {
+
+}
+
+class Solution57 {
+	public int[] solution(String[] name, int[] yearning, String[][] photo) {
+		int[] answer = new int[photo.length];
+
+		for (int i = 0; i < photo.length; i++) {
+			for (int j = 0; j < photo[i].length; j++) {
+				for (int k = 0; k < name.length; k++) {
+					if (photo[i][j].equals(name[k])) {
+						answer[i] = answer[i] + yearning[k];
+						break;
+					}
+				}
+			}
+		}
+		return answer;
+	}
+}
