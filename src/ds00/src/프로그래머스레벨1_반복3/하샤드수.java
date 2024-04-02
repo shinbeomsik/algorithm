f4c4d42e6c99bc875ef86023e8a8199ec0c2725a
shinbeomsik <tnghrltkwnst@naver.com>
2024-04-02 19:50:38 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복3/하샤드수.java
@@ -0,0 +1,25 @@
+package 프로그래머스레벨1_반복3;
+
+
+public class 하샤드수 {
+	   
+}
+
+class Solution10 {
+	public boolean solution(int x) {
+        boolean answer = true;
+        
+        String k = Integer.toString(x);
+        String[] krr = k.split("");
+        
+        int a = 0;
+        
+        for(int i=0;i<krr.length;i++){
+            a = a + Integer.parseInt(krr[i]);
+        }
+        if(x%a !=0){
+            answer = false;
+        }
+        return answer;
+    }
+}
