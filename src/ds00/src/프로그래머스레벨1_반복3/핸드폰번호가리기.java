a724eb78249795919359d1416be83257eae04013
shinbeomsik <tnghrltkwnst@naver.com>
2024-04-04 19:09:16 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복3/핸드폰번호가리기.java
@@ -0,0 +1,19 @@
+package 프로그래머스레벨1_반복3;
+
+
+public class 핸드폰번호가리기 {
+	   
+}
+
+class Solution15 {
+	public String solution(String phone_number) {
+        String answer = "";
+        String[] arr = phone_number.split("");
+        
+        for(int i=0; i<arr.length-4; i++){
+            arr[i] = "*";
+        }
+        answer = String.join("",arr);
+        return answer;
+    }
+}
