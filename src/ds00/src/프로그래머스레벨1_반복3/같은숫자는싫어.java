221a12040b5b483fbc7aee7748a302c6e13cf732
shinbeomsik <124110586+shinbeomsik@users.noreply.github.com>
2024-04-08 22:37:36 +0900

    Update 같은숫자는싫어.java

--- src/ds00/src/프로그래머스레벨1_반복3/같은숫자는싫어.java
+++ src/ds00/src/프로그래머스레벨1_반복3/같은숫자는싫어.java
@@ -8,7 +8,8 @@ public class 같은숫자는싫어 {
 class Solution24 {
 	 public List<Integer> solution(int []arr) {
 	        List<Integer> answer = new ArrayList<>();
-	        
+
+		 
 	        answer.add(arr[0]);
 	        for(int i=1; i<arr.length; i++){
 	            if(arr[i] != arr[i-1]){
