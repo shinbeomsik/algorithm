01a32c64644e6500ee7aff9b8e6b92a4ebf3b53b
shinbeomsik <tnghrltkwnst@naver.com>
2024-04-22 22:51:06 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복4/문자열을정수로바꾸기.java
@@ -0,0 +1,15 @@
+package 프로그래머스레벨1_반복4;
+
+
+public class 문자열을정수로바꾸기 {
+	   
+}
+
+class Solution2 {
+	  public int solution(String s) {
+	        int answer = 0;
+	        
+	        answer = Integer.parseInt(s);
+	        return answer;
+	    }
+}
