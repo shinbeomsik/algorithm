d041a6c92016e118217c9392fcd6bbfb68598f27
shinbeomsik <tnghrltkwnst@naver.com>
2024-04-24 21:02:28 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복4/x만큼간격이있는n개의숫자.java
@@ -0,0 +1,17 @@
+package 프로그래머스레벨1_반복4;
+
+
+public class x만큼간격이있는n개의숫자 {
+	   
+}
+
+class Solution10 {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        
+        for(int i=0;i<n;i++){
+            answer[i] =(long)x*(i+1);
+        }
+        return answer;
+    }
+}
