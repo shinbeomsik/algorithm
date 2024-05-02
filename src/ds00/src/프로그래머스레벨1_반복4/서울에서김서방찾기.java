2d6b73af753d2afcdefb4304a7587b34f7b5ed10
shinbeomsik <tnghrltkwnst@naver.com>
2024-05-02 21:19:22 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복4/서울에서김서방찾기.java
@@ -0,0 +1,21 @@
+package 프로그래머스레벨1_반복4;
+
+
+public class 서울에서김서방찾기 {
+	   
+}
+
+class Solution13 {
+    public String solution(String[] seoul) {
+        String answer = "";
+        
+        for(int i=0; i<seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                answer = "김서방은 " + i + "에 있다";
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
