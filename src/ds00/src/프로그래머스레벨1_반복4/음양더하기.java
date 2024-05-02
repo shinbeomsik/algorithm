41f919db54953caafb3c2f6f9db05ce16c5573ff
shinbeomsik <tnghrltkwnst@naver.com>
2024-05-03 01:32:24 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복4/음양더하기.java
@@ -0,0 +1,20 @@
+package 프로그래머스레벨1_반복4;
+
+
+public class 음양더하기 {
+	   
+}
+
+class Solution14 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i=0;i<signs.length;i++){
+            if(signs[i]){
+                answer+= absolutes[i];
+            }else{
+                answer-= absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
