ad5e9a335cddfb6a88e0bdc7baef15a4e5289a40
shinbeomsik <tnghrltkwnst@naver.com>
2024-05-06 22:54:35 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복4/자연수뒤집어배열로만들기.java
@@ -0,0 +1,20 @@
+package 프로그래머스레벨1_반복4;
+
+
+public class 자연수뒤집어배열로만들기 {
+	   
+}
+
+class Solution5 {
+	public int[] solution(long n) {   
+        String[] arr = Long.toString(n).split("");
+        
+        int[] answer = new int[arr.length];
+        
+        for(int i=0;i<arr.length;i++){
+            answer[i] = Integer.parseInt(arr[arr.length-1-i]);
+        }
+        
+        return answer;
+    }
+}
