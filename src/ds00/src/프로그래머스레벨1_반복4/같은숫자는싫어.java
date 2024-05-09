a6186287b2778e15ff8167792e6ec7d3791f8c93
shinbeomsik <tnghrltkwnst@naver.com>
2024-05-10 00:04:10 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복4/같은숫자는싫어.java
@@ -0,0 +1,29 @@
+package 프로그래머스레벨1_반복4;
+
+import java.util.*;
+public class 같은숫자는싫어 {
+	   
+}
+
+class Solution29 {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        
+        ArrayList<Integer> krr = new ArrayList<>();
+        
+        krr.add(arr[0]);
+        
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] != arr[i-1]){
+                krr.add(arr[i]);
+            }
+        }
+        
+        answer = new int[krr.size()];
+        for(int i=0;i<krr.size();i++){
+            answer[i] = krr.get(i);
+        }
+
+        return answer;
+    }
+}
