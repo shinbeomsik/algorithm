66a86ff2a60b75f18f6ca2c3cd1f14b2ccb9f99c
shinbeomsik <tnghrltkwnst@naver.com>
2024-05-11 23:07:33 +0900

    알고리즘 1레벨 풀이 반복 ﻿

---
+++ src/ds00/src/프로그래머스레벨1_반복4/a3진법뒤집기.java
@@ -0,0 +1,28 @@
+package 프로그래머스레벨1_반복4;
+
+
+public class a3진법뒤집기 {
+	   
+}
+
+class Solution31 {
+	public int solution(int n) {
+        int answer = 0;
+        String k = "";
+        while(n != 0){
+            k =  k + Integer.toString(n%3);
+            n = n/3;
+        }
+        
+        String[] arr = k.split("");
+        int s3 = 1;
+        for(int i=0; i<arr.length; i++){
+            answer = answer + Integer.parseInt(arr[arr.length-1-i]) * s3 ;
+            
+            s3 = s3 * 3;
+            
+        }
+ 
+        return answer;
+    }
+}
