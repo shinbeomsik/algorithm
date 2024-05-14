1547d471d1d043cb0b0b33ef081d1821e691fad4
shinbeomsik <tnghrltkwnst@naver.com>
2024-05-14 21:00:51 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복4/약수의개수와덧셈.java
@@ -0,0 +1,26 @@
+package 프로그래머스레벨1_반복4;
+
+
+public class 약수의개수와덧셈 {
+	   
+}
+
+class Solution23 {
+    public int solution(int left, int right) {
+        int answer = 0;
+        for(int i = left; i<=right;i++){
+            int count =0;
+            for(int j=1;j<=i;j++ ){
+                if(i%j==0){
+                    count++;
+                }
+            }
+            if(count %2 ==0){
+                answer = answer + i;
+            }else{
+                answer = answer - i;
+            }
+        }
+        return answer;
+    }
+}
