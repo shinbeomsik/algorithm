ef416f11db26e036ee007a66ad69096cb264694a
shinbeomsik <tnghrltkwnst@naver.com>
2024-05-18 00:21:49 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복4/이상한문자만들기.java
@@ -0,0 +1,26 @@
+package 프로그래머스레벨1_반복4;
+
+
+public class 이상한문자만들기 {
+	   
+}
+
+class Solution33 {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split(" ",-1);
+        for(int i=0; i<arr.length; i++){
+            String[] krr = arr[i].split("");
+            for(int j=0; j<krr.length; j++){
+                if(j % 2 ==0){
+                    krr[j] = krr[j].toUpperCase();
+                }else{
+                    krr[j] = krr[j].toLowerCase();
+                }
+            }
+            arr[i] = String.join("" , krr);
+        }
+        answer = String.join(" " , arr);
+        return answer;
+    }
+}
