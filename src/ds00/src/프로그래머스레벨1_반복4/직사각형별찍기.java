ce69996dc5a66632a1ced558990e45f8d4b894fd
shinbeomsik <tnghrltkwnst@naver.com>
2024-05-19 00:42:37 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복4/직사각형별찍기.java
@@ -0,0 +1,23 @@
+package 프로그래머스레벨1_반복4;
+import java.util.Scanner;
+
+public class 직사각형별찍기 {
+	   
+}
+
+class Solution28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String star = "";
+        for(int i=0;i<a;i++){
+            star = star + "*";
+        }
+        
+        for(int j=0;j<b;j++){
+            System.out.println(star);
+        }
+   
+    }
+}
