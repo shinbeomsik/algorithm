d1e975f9344ef130d98e739c6ff44e88a5dbda50
shinbeomsik <tnghrltkwnst@naver.com>
2024-05-20 00:44:04 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복4/예산.java
@@ -0,0 +1,23 @@
+package 프로그래머스레벨1_반복4;
+import java.util.Arrays;
+
+public class 예산 {
+	   
+}
+
+class Solution32 {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        
+        for(int i=0; i<d.length; i++){
+            budget = budget - d[i];
+            if(budget >=0){
+                answer++;
+            }else{
+                break;
+            }
+        }
+        return answer;
+    }
+}
