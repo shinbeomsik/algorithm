4d0be8e5c782d16c74819752dc9532a3395a46eb
shinbeomsik <tnghrltkwnst@naver.com>
2024-05-23 00:05:50 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복4/크기가작은부분문자열.java
@@ -0,0 +1,20 @@
+package 프로그래머스레벨1_반복4;
+
+
+public class 크기가작은부분문자열 {
+	   
+}
+
+class Solution35 {
+    public int solution(String t, String p) {
+        int answer = 0;
+        for(int i=0; i<t.length() - p.length()+1; i++){
+            String k = t.substring(i,i+p.length());
+            
+            if(Long.parseLong(p) >= Long.parseLong(k)){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
