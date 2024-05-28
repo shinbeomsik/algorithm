5f1ff70a77dd3aeba61959c21bac14a2eaf699d0
shinbeomsik <tnghrltkwnst@naver.com>
2024-05-28 19:53:08 +0900

    알고리즘 1레벨 풀이 반복

---
+++ src/ds00/src/프로그래머스레벨1_반복4/가장가까운같은글자.java
@@ -0,0 +1,26 @@
+package 프로그래머스레벨1_반복4;
+
+
+public class 가장가까운같은글자 {
+	   
+}
+
+class Solution38 {
+	public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        
+        String[] arr = s.split("");
+        
+        for(int i=0;i<arr.length;i++){
+            answer[i] = -1;
+            for(int j=i-1; j>=0;j--){
+                if(arr[i].equals(arr[j])){
+                    answer[i] = i-j;
+                    break;
+                }
+            }
+            System.out.println("       ");     
+        }
+        return answer;
+    }
+}
