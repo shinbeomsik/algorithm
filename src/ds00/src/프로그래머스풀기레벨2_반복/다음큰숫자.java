ce9f054b4f9c58f22167ddeaf7cc1f9990f8f858
shinbeomsik <tnghrltkwnst@naver.com>
2024-06-01 18:17:44 +0900

    알고리즘 풀이

--- src/ds00/src/프로그래머스풀기레벨2_반복/다음큰숫자.java
+++ src/ds00/src/프로그래머스풀기레벨2_반복/다음큰숫자.java
@@ -23,10 +23,27 @@ class Solution8 {
 }
 
 
-/* 이것도 되긴 하는데 효율성이 안나온다 !!!
+/* 이것도 되긴 하는데 효율성이 안나온다 !!! 정확성은 100프로 통과
  * public int solution(int n) { int answer = 0; boolean as = true; String k =
  * Integer.toBinaryString(n); k = k.replaceAll("0" , ""); int length1 =
  * k.length(); while(as){ n++; k = Integer.toBinaryString(n); k =
  * k.replaceAll("0" , ""); int re1length = k.length(); if(length1 == re1length){
  * as = false; } } answer = n; return answer; }
+ */
+
+/* 이것도 되긴하는데 효율성이 안나온다 !!! 정확성은 100프로 통과
+ * public int solution(int n) { int answer = 0; String s =
+ * Integer.toBinaryString(n); int slength = s.length(); s = s.replaceAll("1" ,
+ * ""); slength = slength - s.length();
+ * 
+ * while(true){ n++;
+ * 
+ * String k = Integer.toBinaryString(n); int klength = k.length(); k =
+ * k.replaceAll("1" , ""); klength = klength - k.length();
+ * 
+ * if(slength == klength){ break; } }
+ * 
+ * answer = n;
+ * 
+ * return answer; }
  */
