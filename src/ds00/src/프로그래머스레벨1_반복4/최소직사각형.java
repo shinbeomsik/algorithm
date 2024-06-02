be2b6d020a68579bef73433cfbff4e4aebd8986c
shinbeomsik <tnghrltkwnst@naver.com>
2024-06-02 22:49:45 +0900

    알고리즘 풀이

---
+++ src/ds00/src/프로그래머스레벨1_반복4/최소직사각형.java
@@ -0,0 +1,27 @@
+package 프로그래머스레벨1_반복4;
+import java.util.Arrays;
+
+public class 최소직사각형 {
+	   
+}
+
+class Solution36 {
+public int solution(int[][] sizes) {
+        
+        int answer = 0;
+
+        int[] maxw = new int[sizes.length];
+        int[] maxh = new int[sizes.length];
+
+        for(int i=0; i<sizes.length; i++){   
+            Arrays.sort(sizes[i]);
+            maxw[i] = sizes[i][0];
+            maxh[i] = sizes[i][1];
+        }
+        Arrays.sort(maxw);
+        Arrays.sort(maxh);
+        
+        answer = maxh[maxh.length-1] * maxw[maxw.length-1];
+        return answer;
+    }
+}
--- src/ds00/src/프로그래머스풀기레벨2_반복/숫자의표현.java
+++ src/ds00/src/프로그래머스풀기레벨2_반복/숫자의표현.java
@@ -6,20 +6,30 @@ public class 숫자의표현 {
 }
 
 class Solution5 {
+	/* 둘이 처리 시간은 비슷하다
+	 * public int solution(int n) { int answer = 0; for(int i=1;i<=n ;i++){ int sum
+	 * =0; for(int j=i; j<=n;j++){ sum = sum + j;
+	 * 
+	 * if(sum == n){ answer++; break; }else if(sum > n ){ break; } } } return
+	 * answer; }
+	 */
+	
 	public int solution(int n) {
         int answer = 0;
-        for(int i=1;i<=n ;i++){
-            int sum =0;
-            for(int j=i; j<=n;j++){
-                sum = sum + j;
+        int count = 1;
+        while(count != n+1){
+            int total =0;
+            for(int i=count; i<=n; i++){
+                total = total + i;
                 
-                if(sum == n){
+                if(total == n){
                     answer++;
-                    break;
-                }else if(sum > n ){
+                }else if(total > n){
                     break;
                 }
-            }
+            } 
+        
+            count++;
         }
         return answer;
     }
