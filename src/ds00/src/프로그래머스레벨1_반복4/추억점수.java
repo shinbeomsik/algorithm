e04b1130540b0a197040276d670e93e1bfecaf2b
shinbeomsik <tnghrltkwnst@naver.com>
2024-06-15 19:08:48 +0900

    알고리즘 풀이

---
+++ src/ds00/src/프로그래머스레벨1_반복4/추억점수.java
@@ -0,0 +1,25 @@
+package 프로그래머스레벨1_반복4;
+
+
+public class 추억점수 {
+	   
+}
+
+class Solution43 {
+	public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        for(int i=0; i<photo.length;i++){
+            for(int j=0; j<photo[i].length;j++){
+                for(int k=0; k<name.length; k++){
+                    if(photo[i][j].equals(name[k])){
+                        answer[i] = answer[i] + yearning[k];
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
