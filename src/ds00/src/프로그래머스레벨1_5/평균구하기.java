9c3c89b671923e9aa3e5a2af6707901d9d7b449d
shinbeomsik <tnghrltkwnst@naver.com>
2024-06-23 22:28:32 +0900

    알고리즘 풀이

---
+++ src/ds00/src/프로그래머스레벨1_5/평균구하기.java
@@ -0,0 +1,15 @@
+package 프로그래머스레벨1_5;
+
+
+public class Z문제틀기본형 {
+			  
+}
+
+class Solution {
+	public int solution(int angle) {
+		int answer = 0;
+	
+		
+		return answer;
+	}
+}
---
+++ src/ds00/src/프로그래머스레벨1_5/하샤드수.java
@@ -0,0 +1,10 @@
+package 프로그래머스레벨1_5;
+
+class Solution {
+	public int solution(int angle) {
+		int answer = 0;
+	
+		
+		return answer;
+	}
+}
