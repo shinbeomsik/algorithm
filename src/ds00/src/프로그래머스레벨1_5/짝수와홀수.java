d81376504f8b61d00bce016d874a0df9a7e81ed3
shinbeomsik <tnghrltkwnst@naver.com>
2024-06-29 23:38:44 +0900

    알고리즘 풀이

---
+++ src/ds00/src/프로그래머스레벨1_5/짝수와홀수.java
@@ -0,0 +1,18 @@
+package 프로그래머스레벨1_5;
+
+
+public class 짝수와홀수 {
+			  
+}
+
+class Solution8 {
+	public String solution(int num) {
+        String answer = "";
+        if(num % 2 ==0){
+            answer = "Even";
+        }else{
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
