e58903b47a2e948d092b6769d31a02d3c1f72fe8
shinbeomsik <tnghrltkwnst@naver.com>
2024-07-09 22:32:43 +0900

    알고리즘 풀이

--- src/ds00/src/프로그래머스레벨1_5/핸드폰번호가리기.java
+++ src/ds00/src/프로그래머스레벨1_5/핸드폰번호가리기.java
@@ -1,10 +1,15 @@
-package 프로그래머스레벨1_5;
+package ds00.src.프로그래머스레벨1_5;
+class 핸드폰번호가리기 {
+	public String solution(String phone_number) {
+		String answer = "";
 
-class Solution {
-	public int solution(int angle) {
-		int answer = 0;
-	
-		
+		String[] arr = phone_number.split("");
+
+		for(int i=0; i<arr.length-4;i++){
+			arr[i] = "*";
+		}
+
+		answer = String.join("",arr);
 		return answer;
 	}
 }
